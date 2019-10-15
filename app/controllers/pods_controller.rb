f313c17fea608cf0584cc99f56c821d8c4bcd040
0x639d1dec74 <guhtymailru@mailinator.com>
2019-10-15 18:33:48 +0300

    open-uri удалён для безопасности. Коммент и notes:todo добавлен для того, чтобы понять в какой момент сработает фильтр.

--- app/controllers/pods_controller.rb
+++ app/controllers/pods_controller.rb
@@ -2,14 +2,14 @@
 
 class PodsController < ApplicationController
   def index
-    @pods = Pod.all
+    @pods = Pod.all # TODO: Filter '' also need to be passed by default
     elements = []
     @pods.each_with_index do |pod, num|
-      if pod.id == @pod_ids
-        elements << "show"
-      else
-        elements << "view"
-      end
+      elements << if pod.id == @pod_ids[num]
+                    'show'
+                  else
+                    'view'
+                  end
     end
   end
 end
--- app/models/pod.rb
+++ app/models/pod.rb
@@ -1,33 +1,33 @@
 # frozen_string_literal: true
 
 class Pod < ApplicationRecord
-  require 'open-uri'
-
-  URL = 'https://data.gov.ru/organizations'
+  URI = 'https://data.gov.ru/organizations'
 
   has_many :datum
 
-  def initialize; end
+  def initialize
+    doc = Nokogiri::HTML(URI.parse(URL), nil, 'UTF-8')
+  end
 
   # Filter
-  # If button pressed it sends get-query. Scrape will update showing table.
-  # def self.for(keyword)
-  #   get('/search', query: { q: keyword })['pods'] # response:  count: value pods: value
+  # Sends post-query to URI. Here scrape updates variarles.
+  # def filter(keyword)
+  #   get('/search', query: { q: keyword })['pods']
   #   scrape
   # end
 
-private
+  private
 
   def scrape
     @pod_names = []
     @pod_ids = []
-    doc = Nokogiri::HTML(open(URL), nil, 'UTF-8')
+
     doc = doc.xpath('//tbody')
     doc.xpath('//td[2]/a').each do |anchor|
       @pod_names << anchor.text
 
-    # gsub do deletion of /organization/ - it remains only taxpayer's id
-      @pod_ids << anchor.xpath('@href').to_s.gsub(/(\/\w+\/)(\d+)/, '\2')
+      # gsub do deletion of /organization/ - it remains only taxpayer's id
+      @pod_ids << anchor.xpath('@href').to_s.gsub(%r{(/\w+/)(\d+)}, '\2')
     end
   end
 end
