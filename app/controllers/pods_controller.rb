abe18d3dde9f99bf7170d049735ca2d398653d83
0x639d1dec74 <guhtymailru@mailinator.com>
2019-10-22 11:44:28 +0300

    Модель для скапинга-навигации создана. В неё вынесено всё из контроллера ПОД-ов.

--- app/controllers/pods_controller.rb
+++ app/controllers/pods_controller.rb
@@ -2,7 +2,6 @@
 
 class PodsController < ApplicationController
   before_action :set_pod, only: %i[show edit update]
-
   # ---------------set_pod------------------------------------------------------
   def show
     @tax_payers_id = params[:tax_payer_id]
@@ -15,11 +14,15 @@ class PodsController < ApplicationController
 
   # ---------------set_pod, scrape, elem_retrieval------------------------------
   def index
-    @pods = Pod.all
-    @cur_page ||= 0
-    # TODO: CACHE VIEWS IF CUR_PAGE=0
-    scrape(@cur_page)
-    @elements = elem_retrieval(@pod_ids, @pods)
+    @index_cur_page ||= 0
+    # TODO: add memoization of @page_nav and reimplemet to call from here
+    #   as method and this method must have Thread.new inside.
+    @page_nav = PageNav.new(@index_cur_page)
+    @index_cur_page = @page_nav.current_page
+    @index_last_page = @page_nav.last_page
+
+    @page_nav.scrape(@index_cur_page)
+    @elements = elem_retrieval(@page_nav.pod_ids, Pod.all)
   end
 
   # ---------------scrape_data, pod_params--------------------------------------
@@ -28,7 +31,7 @@ class PodsController < ApplicationController
     @pod = Pod.new(pod_params)
     @pod.save
     # @pod.data.build
-    scrape_data(@pod) # @tax_payer_id
+    @page_nav.scrape_data(@pod) # @tax_payer_id
 
     if @pod.save
       respond_to do |format|
@@ -55,39 +58,9 @@ class PodsController < ApplicationController
     # , data_attributes: [:id, :date:, source, :author, :converted])
   end
 
-  # rubocop:disable Metrics/AbcSize
-  # TODO: Filter '' needs to be passed by default.
-  # TODO: base_url with the page number query need to be implemented.
-  def scrape(_cur_page = 0)
-    require 'open-uri'
-
-    base_url = 'https://data.gov.ru/organizations'
-    doc = Nokogiri::HTML(URI.parse(base_url).open, nil, 'UTF-8')
-    @pod_organizations = []
-    @pod_ids = []
-
-    # If we start browsing base_url, li[12] will be the last paginated table
-    if @cur_page.zero? && @last_page.nil?
-      @last_page = doc
-                   .xpath('//div[3]/ul/li[12]/a')
-                   .to_s
-                   .gsub(/^.*=/, '') # removes unuseful parts of url query
-                   .gsub(/\D/, '').to_i # removes non digit parts of html tags
-    end
-
-    doc = doc.xpath('//tbody')
-    doc.xpath('//td[2]/a').each do |anchor|
-      @pod_organizations << anchor.text.strip
-
-      # gsub does deletion of /organization/ - it remains only taxpayer's id:
-      @pod_ids << anchor.xpath('@href').to_s.gsub(%r{(/\w+/)(\d+)}, '\2').to_s
-    end
-  end
-  # rubocop:enable Metrics/AbcSize
-
   # Attributes: @pod_ids => tax_payer_id_numbers_on_page,
   #              array of scrapped tax_payer's id numbers
-  #             @pods => pods
+  #             Pod.all => pods
   def elem_retrieval(tax_payer_id_numbers_on_page, pods)
     elements = []
     tax_payer_id_numbers_on_page.each do |pod|
@@ -99,6 +72,4 @@ class PodsController < ApplicationController
     end
     elements
   end
-
-  def scrape_data(pod); end
 end
---
+++ app/models/page_nav.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+class PageNav
+  include ActiveModel::Model
+  attr_reader :page
+  attr_accessor :limit, :pod_organizations, :pod_ids, :current_page, :last_page
+
+  def initialize(first_page)
+    pod_organizations ||= []
+    pod_ids ||= []
+    page = first_page.to_i
+    last_page = page
+    limit = 20
+  end
+
+  # Strange implementation of pod_organizations and pod_ids caching
+  #   for views if we don't changing page but pushing 'Visit page' button.
+  #            (controllers/pods/index.html.slim)
+  def page=(new_page)
+    break if new_page == page
+
+    pod_organizations = []
+    pod_ids = []
+    @page = new_page
+  end
+
+  def to_model
+    self
+  end
+
+  def to_key
+    page
+  end
+
+  def persisted?
+    false
+  end
+
+  def scrape_data(pod); end
+
+  # rubocop:disable Metrics/AbcSize
+  # TODO: Filter '' needs to be passed by default.
+  # TODO: base_url with the page number query need to be implemented.
+  def scrape(cur_page)
+    require 'open-uri'
+
+    base_url = 'https://data.gov.ru/organizations'
+    doc = Nokogiri::HTML(URI.parse(base_url).open, nil, 'UTF-8')
+
+    # TODO: TEST crawling when base_url have only one page (expect @last_page.to be 0)
+    # If we start browsing base_url, li[12] will be the last paginated table
+    if page.zero? && cur_page.zero?
+      last_page = doc
+                  .xpath('//div[3]/ul/li[12]/a')
+                  .to_s
+                  .gsub(/^.*=/, '') # removes unuseful parts of url query
+                  .gsub(/\D/, '').to_i # removes non digit parts of html tags
+    end
+
+    doc = doc.xpath('//tbody')
+    doc.xpath('//td[2]/a').each do |anchor|
+      pod_organizations << anchor.text.strip
+
+      # gsub does deletion of /organization/ - it remains only taxpayer's id:
+      pod_ids << anchor.xpath('@href').to_s.gsub(%r{(/\w+/)(\d+)}, '\2').to_s
+    end
+  end
+  # rubocop:enable Metrics/AbcSize
+end
