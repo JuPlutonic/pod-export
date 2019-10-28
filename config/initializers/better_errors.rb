5d8979f26e26c1d90114d78fa1d9be0c80623467
0x639d1dec74 <guhtymailru@mailinator.com>
2019-10-28 17:04:10 +0300

    Проблема использования BetterErrors#1.

--- config/initializers/better_errors.rb
+++ config/initializers/better_errors.rb
@@ -3,4 +3,4 @@
 # to the length of #inspect's payload for the given object. Please be aware that HTML escaping
 # modifies the size of this payload so setting this limit too precisely is not recommended.
 # default value: 100_000
-BetterErrors.maximum_variable_inspect_size = 2_900_000
+BetterErrors.maximum_variable_inspect_size = 2_900_000 if ENV['RAILS_ENV'] == 'development'
