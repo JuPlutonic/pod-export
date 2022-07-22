c9e11fff6f702d77d13594f05429b4b351b432dd
JuPlutonic <JuPlutonic@users.noreply.github.com>
2022-07-22 21:42:54 +0300

    Spec changed

--- spec/operations/tax_payer_id_operation_spec.rb
+++ spec/operations/tax_payer_id_operation_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require 'byebug'
 
 RSpec.describe TaxPayerIdOperation do
   describe '#call' do
@@ -9,8 +10,12 @@ RSpec.describe TaxPayerIdOperation do
         expect(described_class.new('0000000000').call).to be_falsey
       end
 
-      it 'is true for the proper tax_payer_id' do
-        expect(described_class.new(Faker::Russian.inn).call).to be true
+      it 'is true for one recoverable tax_payer_id' do
+        expect(described_class.new('8901017082-0').call).to be true
+      end
+
+      it 'is true for another recoverable tax_payer_id' do
+        expect(described_class.new('005247051127').call).to be true
       end
     end
   end
